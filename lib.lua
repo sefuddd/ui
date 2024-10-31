fb700232e08a243780ab39d1947bf973d7e7f9a2
sefuddd <54879573+sefuddd@users.noreply.github.com>
2024-10-31 10:03:02 -0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -353,6 +353,7 @@ function UIlib:Janela()
             newDropdown.Active = false
             local sizeX, sizeY = 100, 50
             newDropdown.Button.Size = UDim2.new(0, sizeX, 0, sizeY)
+            newDropdown.Position = UDim2.new(0, 5, 0, #TabContent:GetChildren() * 45)  -- Posição vertical
             newDropdown.Button.Text = config.Options[1] -- Define o texto inicial como a primeira opção
             newDropdown.Button.Parent = TabContent
         
